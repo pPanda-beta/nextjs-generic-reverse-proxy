439179f18aa05e71ba142375692d431e9ca9e456
Palash Das <dpalash.new@gmail.com>
2020-11-03 04:40:22 +0530

    Expose cookies for frontend js as well

--- src/pages/api/activate.js
+++ src/pages/api/activate.js
@@ -1,12 +1,16 @@
 import {serialize as serializeCookie} from 'cookie';
 import {BACKEND_COOKIE_NAME} from "../../configs";
-
+import {ProxyBackend} from "../../models";
 
 export default (req, res) => {
   const serializedBackend = req.query.backend;
-  const cookie = serializeCookie(BACKEND_COOKIE_NAME, serializedBackend,
+  const proxyCookie = serializeCookie(BACKEND_COOKIE_NAME, serializedBackend,
       {httpOnly: true, path: '/'});
 
-  res.setHeader('Set-Cookie', cookie);
+  const proxyBackend = ProxyBackend.fromBase64(serializedBackend);
+  const stateCookiesForFrontendJS = Object.entries(proxyBackend.cookies).map(
+      ([k, v]) => serializeCookie(k, v, {path: '/'}));
+
+  res.setHeader('Set-Cookie', [proxyCookie, ...stateCookiesForFrontendJS]);
   res.redirect('/');
 };
